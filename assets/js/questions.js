c3f607dcf9267fe1a9db530297d69bbb399c0b23
carl.dunne <developerdunne@gmail.com>
2023-11-21 15:07:46 +0000

    Added header and footer and modal

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,19 +1,3 @@
-/*jshint esversion: 6 */
-
-//Waiting for DOM to load before game starts.
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByTagName("button");
-  for (let button of buttons) {
-    button.addEventListener("click", function () {
-      if (this.getAttribute("data-type") === "start") {
-        quizPage.classList.remove("hide");
-        instructionsPage.classList.add("hide");
-        startQuiz();
-      }
-    });
-  }
-});
-
 //Questions & Answers.
 const questions = [
   {
@@ -134,7 +118,7 @@ const questions = [
     ],
   },
   {
-    question: "Which planet is farthest from the sun",
+    question: "Which planet is farthest from the sun?",
     answers: [
       { text: "Pluto", correct: true },
       { text: "Mercury", correct: false },
@@ -152,95 +136,3 @@ const questions = [
     ],
   },
 ];
-
-const instructionsPage = document.getElementById("instructions");
-const quizPage = document.getElementById("quiz-box");
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-
-questions.sort(() => Math.random() - 0.5).slice(0, 10);
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-//Starts Quiz.
-function startQuiz() {
-  currentQuestionIndex = 0;
-  score = 0;
-  showQuestion();
-}
-
-//Displays Questions.
-function showQuestion() {
-  resetState();
-  let currentQuestion = questions[currentQuestionIndex];
-  let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-
-  currentQuestion.answers.forEach((answer) => {
-    const button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("btn");
-    answerButtons.appendChild(button);
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
-    button.addEventListener("click", selectAnswer);
-  });
-}
-function resetState() {
-  nextButton.style.display = "none";
-  while (answerButtons.firstChild) {
-    answerButtons.removeChild(answerButtons.firstChild);
-  }
-}
-//Shows score at end of quiz.
-function showScore() {
-  resetState();
-  if (score <= 6) {
-    questionElement.innerHTML = `You scored ${score} out of 10! Keep practising!`;
-  } else if (score <= 8) {
-    questionElement.innerHTML = `You scored ${score} out of 10! Good job!`;
-  } else if (score === 10) {
-    questionElement.innerHTML = `You scored ${score} out of 10! We have lift off!!`;
-  }
-  nextButton.innerHTML = "Play Again?";
-  nextButton.style.display = "block";
-}
-
-//When an answer is selected.
-function selectAnswer(e) {
-  const selectedBtn = e.target;
-  const answerCorrect = selectedBtn.dataset.correct === "true";
-  if (answerCorrect) {
-    selectedBtn.classList.add("correct");
-    score++;
-  } else {
-    selectedBtn.classList.add("incorrect");
-  }
-  Array.from(answerButtons.children).forEach((button) => {
-    if (button.dataset.correct === "true") {
-      button.classList.add("correct");
-    }
-    button.disabled = true;
-  });
-  nextButton.style.display = "block";
-}
-function handleNextButton() {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < 10) {
-    showQuestion();
-  } else {
-    showScore();
-  }
-}
-nextButton.addEventListener("click", () => {
-  if (currentQuestionIndex < 10) {
-    handleNextButton();
-  } else {
-    startQuiz();
-  }
-});
-
-startQuiz();
---
+++ assets/js/script.js
@@ -0,0 +1,133 @@
+//Waiting for DOM to load before game starts.
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "start") {
+        quizPage.classList.remove("hide");
+        instructionsPage.classList.add("hide");
+        startQuiz();
+      }
+    });
+  }
+});
+
+const instructionsPage = document.getElementById("instructions");
+const quizPage = document.getElementById("quiz-box");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+//Randomises questions.
+
+questions.sort(() => Math.random() - 0.5).slice(0, 10);
+
+//Starts Quiz.
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  showQuestion();
+}
+
+//Displays Questions.
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+//Shows score at end of quiz.
+function showScore() {
+  resetState();
+  if (score <= 6) {
+    questionElement.innerHTML = `You scored ${score} out of 10! Keep practising!`;
+  } else if (score <= 8) {
+    questionElement.innerHTML = `You scored ${score} out of 10! Good job!`;
+  } else if (score === 10) {
+    questionElement.innerHTML = `You scored ${score} out of 10! We have lift off!!`;
+  }
+  nextButton.innerHTML = "Play Again?";
+  nextButton.style.display = "block";
+}
+
+//When an answer is selected.
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const answerCorrect = selectedBtn.dataset.correct === "true";
+  if (answerCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < 10) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < 10) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+startQuiz();
